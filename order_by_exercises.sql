b3e9ed6c946fd84fa26e438228bcbfb6600f9f4a
james McBride <james.m.mcbride15@gmail.com>
2021-02-26 10:10:18 -0600

    added mysql order by clauses exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,17 +1,17 @@
 USE employees;
-
-SELECT * FROM employees WHERE (first_name='Irena' OR first_name='Vidya' OR first_name='Maya' )AND gender='F';
+SELECT * FROM EMPLOYEES ORDER BY last_name, first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya' ) ORDER BY last_name, first_name;
 
 SELECT * FROM employees WHERE last_name like 'E%' OR last_name like '%E' ;
 
-SELECT * FROM employees WHERE last_name like 'E%' AND last_name like '%E' ;
+SELECT * FROM employees WHERE last_name like 'E%' AND last_name like '%E' ORDER BY emp_no DESC;
 
 
 SELECT * from employees where hire_date like '199%';
 
 SELECT * from employees where birth_date like '%12-25';
 
-SELECT * from employees where birth_date like '%12-25' AND hire_date like '199%';
+SELECT * from employees where birth_date like '%12-25' AND hire_date like '199%' ORDER BY birth_date, hire_date DESC;
 
 
 SELECT * from employees where last_name like '%q%' AND last_name NOT LIKE '%qu%';
